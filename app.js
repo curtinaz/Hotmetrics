810f2c3b265904acaedded859fb41432a417edd9
Cristian Curtinaz <63929112+curtinaz@users.noreply.github.com>
2021-03-16 14:23:57 -0300

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // Route that receives a POST request to /sms
 
 app.get('/', function (req, res) {
-  res.render('./views/index.html')
+  res.render('./views/index')
 })
 
 // WEBHOOK DE TESTE
