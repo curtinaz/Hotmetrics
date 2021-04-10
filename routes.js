bc00796fdef3310f9e7f173c36f71ff0e36e9055
Cristian Curtinaz <63929112+curtinaz@users.noreply.github.com>
2021-04-10 17:53:13 -0300

    Update routes.js

--- routes.js
+++ routes.js
@@ -3,3 +3,8 @@ app.get('/', function (req, res) {
     res.render('./public/index.html')
   })
   
+  // Página de exemplo da Hotmetrics
+  app.get('/example', function (req, res) {
+    res.render('./public/example.html')
+  })
+  
