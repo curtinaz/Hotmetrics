7744acb8ca998205b5f8460a058c8abe727abd72
Cristian Curtinaz <63929112+curtinaz@users.noreply.github.com>
2021-04-11 17:53:52 -0300

    Update routes.js

--- routes.js
+++ routes.js
@@ -22,3 +22,16 @@ app.get('/', function (req, res) {
     });
   })
   
+  // WEBHOOK CRIADOR DE CONTAS
+  app.post('/webhooks', function (req, res) {
+    const body = req.body
+    var data = "," + "\n" +JSON.stringify(body)
+    res.set('Content-Type', 'text/plain');
+    res.send(data);
+    
+    console.log(data);
+    
+    fs.appendFile(`./public/message.json`, data, function (err) {
+      if (err) throw err;
+    });
+  })
