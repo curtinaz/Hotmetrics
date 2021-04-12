b105160ca1ede7f209212e176e1e44d38e3f1147
Cristian Curtinaz <63929112+curtinaz@users.noreply.github.com>
2021-04-12 17:56:50 -0300

    Update app.js

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ app.post('/webhook', function (req, res) {
 })
 
 // WEBHOOK CRIADOR DE CONTAS
-app.post('/newuser', function (req, res) {
+app.post('/newuser/:fbid', function (req, res) {
   const body = req.body
   var data = "," + "\n" +JSON.stringify(body)
   res.set('Content-Type', 'text/plain');
