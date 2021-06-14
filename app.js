1e67f76c01478c21c844312716c309ba9186f02f
Cristian Curtinaz <63929112+curtinaz@users.noreply.github.com>
2021-06-14 16:56:18 -0300

    Update app.js

--- app.js
+++ app.js
@@ -55,8 +55,8 @@ app.get('./newuser/:fbid', function (req, res) {
 
 // Faz o app rodar na porta 3000
 
-var port_number = server.listen(process.env.PORT || 3000);
-app.listen(port_number);
+// var port_number = server.listen(process.env.PORT || 3000);
+app.listen(3000);
 console.log('server is running on port 3000')
 
 // app.listen(3000, function (err) {
