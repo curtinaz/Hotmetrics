69daf767b27e2b98c137c02f2a02be330ede9397
Cristian Curtinaz <curtinazif@gmail.com>
2022-05-15 09:44:28 -0300

    Adios config.php

--- dashboard/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-define('HOSTNAME', 'localhost');
-define('USERNAME', 'root');
-define('PASSWORD', '');
-define('DATABASE', 'hotmetrics');
-
-$con = new mysqli(HOSTNAME, USERNAME, PASSWORD, DATABASE);
